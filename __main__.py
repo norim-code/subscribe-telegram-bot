915ae91a11318429bfe5e026250d4a892a1da6d3
norim-code <N0R1M@yandex.ru>
2023-05-08 12:55:13 +0300

    modified:   __main__.py     created all main handlers

--- __main__.py
+++ __main__.py
@@ -10,14 +10,18 @@ logging.basicConfig(level=logging.INFO)
 dp = Dispatcher()
 async def main():
     await dp.start_polling(Bot(token="6235859658:AAFR03q1rqvZDfUtUJfvSvkmLEtzcEDyrro"))
-async def subscribe(message: Message):
+@dp.message(Text("🛒 Тарифы"))
+async def pay_subscribe(message: Message):
     sub_variants_buttons=[
         [
-            InlineKeyboardButton(text="🔶 1 МЕСЯЦ", callback_data="one_month"),
-            InlineKeyboardButton(text="🔶 3 МЕСЯЦА | -15% ВЫГОДА", callback_data="three_month")
+            InlineKeyboardButton(text="🔶 1 МЕСЯЦ", callback_data="sub_one_month"),
+            InlineKeyboardButton(text="🔶 3 МЕСЯЦА | -15% ВЫГОДА", callback_data="sub_three_month")
         ],
     ]
     await message.answer("Выберите желаемый тарифный план:", reply_markup=InlineKeyboardMarkup(inline_keyboard=sub_variants_buttons))
+@dp.message(Text("📊 Моя подписка"))
+async def check_subscribe(message: Message):
+    await message.answer("Акукарача")
 @dp.message(Command("start"))
 async def intro(message: Message):
     main_buttons=ReplyKeyboardMarkup(
@@ -31,6 +35,6 @@ async def intro(message: Message):
         input_field_placeholder="all 4 designer subsribe"
     )
     await message.answer(f"👋  Привет, {message.from_user.first_name}\n  \n   Приватный канал включает в себя все то, что опубликовано в демо канале — @all4designer_private. Это только экслюзивные и редкие материалы.\n \n  По вашим запросам мы можем находить нужные вам материалы в индивидуальном порядке или, если ваш ассет довольно востребованный - мы можем его купить.\n \n    Оплата происходит через сервис Boosty. Это аналог сервиса Patreon. Это единственный и самый дешевый способ перевода средств из-за рубежа в РФ. Этот сервис абсолютно безопасный. К сожалению, из-за санкций в моей стране невозможно принимать платежи из-за границы напрямую (Paypal, Revolut и тому подобные), поэтому использую только этот сервис для международных платежей. Если у вас не получается оплатить этим способом, то я помогу найти другой способ.\n \n    После успешной оплаты  будет отправлена пригласительная ссылка в приватный канал, где у вас будет безграничный доступ ко всем ресурсам.", reply_markup=main_buttons)
-    await subscribe(message)
+    await pay_subscribe(message)
 if __name__=="__main__":
     asyncio.run(main())
